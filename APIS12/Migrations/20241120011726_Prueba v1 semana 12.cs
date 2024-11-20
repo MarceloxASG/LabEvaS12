97ea8de80fd9d2d3c7f14dad8b7f1f473f98baa1
Alumno <Alumno@LAB1507-02>
2024-11-19 20:40:26 -0500

    Agregar archivos de proyecto.

---
+++ APIS12/Migrations/20241120011726_Prueba v1 semana 12.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIS12.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pruebav1semana12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
